3bbc12c791019c936bef8c7a9f3d9d9483d2ac75
Tawhid Hannan <Tawhid Hannan>
2016-08-10 22:54:44 +0100

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { AppComponent } from './app/app.component';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+
+bootstrap(AppComponent);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var htmlInject = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'app': './src/main.ts',
+        'polyfills': './src/polyfills.ts'
+    },
+    output: {
+        path: './dist',
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].map'
+    },
+    module: {
+        loaders: [
+            {test: /\.ts$/, loader: 'ts'}
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    plugins: [
+        new htmlInject({
+            template: './src/index.html'
+        })
+    ]
+}
